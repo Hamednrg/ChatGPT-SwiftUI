038fe3901905c4815fd7760fec8892a81169f5f3
Hamed Naji <hamednajirg@gmail.com>
2023-02-11 23:53:11 -0500

    Initial Commit

---
+++ ChatGPT-SwiftUI/ChatGPT_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPT_SwiftUIApp.swift
+//  ChatGPT-SwiftUI
+//
+//  Created by Hamed Naji on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPT_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
