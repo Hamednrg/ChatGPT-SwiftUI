5f57acf6d4422d83bacdca68009406cdd04a044f
Hamed Naji <hamednajirg@gmail.com>
2023-02-13 19:22:16 -0500

    constants file was created.

---
+++ ChatGPT-SwiftUI/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ChatGPT-SwiftUI
+//
+//  Created by Hamed Naji on 2023-02-12.
+//
+
+import Foundation
+
+enum Constants{
+    static let openAIAPIKey = "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"
+}
